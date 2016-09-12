29a7f2d34bf21cd123151f73ca2d9ebfb6adae64
Luca Bruno <luca.bruno@coreos.com>
2016-09-12 12:58:37 +0000

    adjust makefiles for dynexes

--- makelib/build_static_c_bin.mk
+++ makelib/build_static_c_bin.mk
@@ -3,6 +3,7 @@
 # BSCB_SOURCES: sources used to build a binary
 # BSCB_HEADERS: headers used to build a binary
 # BSCB_ADDITIONAL_CFLAGS: additional CFLAGS passed to CC, just after CFLAGS
+# BSCB_ADDITIONAL_LDFLAGS: additional LDFLAGS passed to CC
 
 # misc inputs (usually provided by default):
 # CC - C compiler
@@ -11,12 +12,12 @@
 _BSCB_PATH_ := $(lastword $(MAKEFILE_LIST))
 
 $(call forward-vars,$(BSCB_BINARY), \
-	CC CFLAGS BSCB_ADDITIONAL_CFLAGS BSCB_SOURCES)
+	CC CFLAGS BSCB_ADDITIONAL_CFLAGS BSCB_ADDITIONAL_LDFLAGS BSCB_SOURCES)
 $(BSCB_BINARY): $(BSCB_SOURCES) $(BSCB_HEADERS)
 $(BSCB_BINARY): $(_BSCB_PATH_)
 	$(VQ) \
 	$(call vb,vt,CC,$(call vsp,$@)) \
-	$(CC) $(CFLAGS) $(BSCB_ADDITIONAL_CFLAGS) -o "$@" $(BSCB_SOURCES) -static -s
+	$(CC) $(CFLAGS) $(BSCB_ADDITIONAL_CFLAGS) -o "$@" $(BSCB_SOURCES) -s $(BSCB_ADDITIONAL_LDFLAGS)
 
 CLEAN_FILES += $(BSCB_BINARY)
 
